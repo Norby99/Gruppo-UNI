6c071632a603043611ca5d29e2609ed21c53b6df
Sara Romeo <57600591+sara-romeo@users.noreply.github.com>
2020-10-07 16:36:44 +0200

    Update main.c

--- Consegna-2/Esercizio-2/main.c
+++ Consegna-2/Esercizio-2/main.c
@@ -1,3 +1,42 @@
+/*AUTHOR: Cristina Zoccola(cristina.zoccola@studio.unibo.it)
+          Denny Reggidori(denny.reggidori@studio.unibo.it)
+          Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
+          Sara Romeo(sara.romeo3@studio.unibo.it)
+
+ DATE:   20201007
+
+ NOTE:   Text Ex02: Pensa a un numero. L’utente pensa ad un numero e il computer lo deve indovinare compiendo le seguenti operazioni.
+	 L’utente fissa un intervallo entro cui generare il valore da indovinare (min, max). Se i valori inseriti non rispettano il vincolo 0<min≤max,
+	 l’operazione di lettura va ripetuta. L’utente pensa ad un numero compreso tra min e max.
+	 Il programma cerca di indovinare la scelta dell’utente e propone un numero. A seconda della situazione l’utente deve dire al programma se
+	 il numero da indovinare è più piccolo; il numero da indovinare è più grande; ha indovinato. Non poniamo limite al numero di tentativi 
+
+	 Due possibili alternative di gioco:
+	 	 1)
+	 	 Inserisci l’intervallo di ricerca (min max): 23 89
+	 	 Pensa ad un numero compreso tra 23 e 89
+	 	 Hai pensato a 56? (s/n) n
+	 	 il numero viene prima (<) o dopo (>) di 56? >
+	 	 Hai pensato a 73? (s/n) n
+	 	 il numero viene prima (<) o dopo (>) di 73? >
+	 	 Hai pensato a 81? (s/n) n
+	 	 il numero viene prima (<) o dopo (>) di 81? <
+	 	 Hai pensato a 77? (s/n) n
+	 	 il numero viene prima (<) o dopo (>) di 77? <
+	 	 Hai pensato a 75? (s/n) s
+	 	 Ho indovinato!!
+
+	 	 2)
+	 	 Inserisci l’intervallo di ricerca (min max): 45 124
+	 	 Pensa ad un numero compreso tra 45 e 124
+	 	 Il numero e’ minore (<) uguale (=) o maggiore (>) di 84? >
+	 	 Il numero e’ minore (<) uguale (=) o maggiore (>) di 104? <
+	 	 Il numero e’ minore (<) uguale (=) o maggiore (>) di 94? >
+	 	 Il numero e’ minore (<) uguale (=) o maggiore (>) di 99? <Il numero e’ minore (<) uguale (=) o maggiore (>) di 96? >
+	 	 Il numero e’ minore (<) uguale (=) o maggiore (>) di 97? =
+	 	 Hai pensato a 97!*/
+ 
+ 
 #include <stdio.h>
 
 int main() {
@@ -20,11 +59,11 @@ int main() {
         printf("Il numero e’ minore (<) uguale (=) o maggiore (>) di %d? ", iaGuess);
         scanf(" %c", &userResponse);
 
-        if(userResponse != '<' && userResponse != '=' && userResponse != '>'){  // Obbligo l'utene ad inserire uno dei tre valori
+        if(userResponse != '<' && userResponse != '=' && userResponse != '>'){  // Obbligo l'utente ad inserire uno dei tre valori
             printf("Valore non valido!\n");
         }
         
-        /* Restrizzione del range dei valori */
+        /* Restrizione del range dei valori */
         else if(userResponse == '<')
                 max = iaGuess-1;
         else if(userResponse == '>')
@@ -32,7 +71,8 @@ int main() {
     }
 
     printf("Il numero a cui hai pensato e' %d", iaGuess);
-
+	
+    system("pause");
     return 0;
 
 }
