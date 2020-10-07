c039d4965d6e564d970151aad76d61bf46b4cbf3
Sara Romeo <57600591+sara-romeo@users.noreply.github.com>
2020-10-07 16:55:14 +0200

    Add files via upload

---
+++ Consegna-2/Esercizio-1/20201013_Elaborato01.c
@@ -0,0 +1,23 @@
+/*AUTHOR: Cristina Zoccola(cristina.zoccola@studio.unibo.it)
+          Denny Reggidori(denny.reggidori@studio.unibo.it)
+          Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
+          Sara Romeo(sara.romeo3@studio.unibo.it)
+ DATE:    20201007
+ NOTE:    Text Ex01: Scrivere un programma C che mostri l’utilizzo di tutti gli operatori studiati.
+          Il programma deve inoltre mostrare il comportamento scorretto che si ottiene utilizzando tipi non appropriati
+          alla dimensione delle variabili, quello che si ottiene usando più operatori con side effects
+          all’interno della stessa istruzione, quello che si ottiene quando le regole di conversione implicita sono applicate
+          e quando si supera il range dei valori disponibili per un tipo di dato.
+          Il programma deve poi mostrare l’utilizzo di tutte le funzioni matematiche della libreria math.h.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+
+
+    system("pause");
+    return 0;
+
+}
