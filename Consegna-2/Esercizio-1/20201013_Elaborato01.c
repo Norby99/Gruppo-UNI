a4e5717c021177f95fa13c6d3367b24b5bc2dff4
DennyReggidori <72309051+DennyReggidori@users.noreply.github.com>
2020-10-09 17:20:29 +0200

    Update 20201013_Elaborato01.c

--- Consegna-2/Esercizio-1/20201013_Elaborato01.c
+++ Consegna-2/Esercizio-1/20201013_Elaborato01.c
@@ -2,7 +2,7 @@
           Denny Reggidori(denny.reggidori@studio.unibo.it)
           Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
           Sara Romeo(sara.romeo3@studio.unibo.it)
- DATE:    20201007
+ DATE:    20201009
  NOTE:    Text Ex01: Scrivere un programma C che mostri l’utilizzo di tutti gli operatori studiati.
           Il programma deve inoltre mostrare il comportamento scorretto che si ottiene utilizzando tipi non appropriati
           alla dimensione delle variabili, quello che si ottiene usando più operatori con side effects
@@ -17,54 +17,11 @@
 void showbits (unsigned int);
 int main()
 {
-    /* ho riportato qui credo tutti gli operatori studiati che dobbiamo usare
-    quelle che ho già inserito ho messo una X a inizio riga
-    
-    OPERATORI ARITMETICI p. 86 lucidi 4
-    X   ++op Incrementa di un'unità l'operando prima che venga restituito il suo valore.
-    X   op++ Incrementa di un'unità l'operando dopo averne restituito il suo valore.
-    X   --op Decrementa di un'unità l'operando prima che venga restituito il suo valore.
-    X   op--Decrementa di un'unità l'operando dopo averne restituito il suo valore.
-    X   +op Non ha alcun effetto(nel K&R non esiste) specifica che una costante numerica è positiva
-    X   -op Inverte il segno dell'operando (prima di restituirne il valore).
-    X   op1 + op2 Somma i due operandi.
-    X   op1 - op2 Sottrae dal primo il secondo operando.
-    X   op1 * op2 Moltiplica i due operandi.
-    X   op1 / op2 Divide il primo operando per il secondo (se operandi interi tronca il risultato).
-    X   op1 % op2 Calcola il resto della divisione tra il primo e il secondo operando, richiede operandi interi.
-    X   var = valore Assegna alla variabile il valore alla destra.
-    X   op1 += op2 op1 = (op1 + op2)
-    X   op1 -= op2 op1 = (op1 -op2)
-    X   op1 *= op2 op1 = (op1 * op2)
-    X   op1 /= op2 op1 = (op1 / op2)
-    X   op1 %= op2 op1 = (op1 % op2)
-    
-    OPERATORI RELAZIONALI
-    < Primo operando minore del secondo operando
-    > Primo operando maggiore del secondo operando
-    <= Primo operando minore o uguale al secondo operando
-    >= Primo operando maggiore o uguale al secondo operando
-    == Primo operando uguale al secondo operando
-    X   != Primo operando non uguale al secondo operando
-    OPERATORI LOGICI
-    ! Calcola il NOT logico dell’operando
-    && Calcola l’AND logico tra i due operandi
-    || Calcola l’OR logico tra i due operandi
-    
-    OPERATORI BITWISE - esempi a p. 112 lucidi 4
-    X   & AND logico bit a bit tra i due operandi
-    X   | OR logico bit a bit tra i due operandi
-    X   ^ XOR logico bit a bit tra i due operandi
-    X   ~ Complemento a 1 dell’operando
-    X   >> Shifta destra del primo operando per un numero di bit pari alvalore del secondo
-    X   << Shifta sinistra del primo operando per un numero di bit pari alvalore del secondo
-    
-    non so se anche
-    sizeof()
+    /*sizeof()
     X   casting
     */
 
-    //usiamo l'operatore di assegnamento nella dichiarazione delle variabili
+     //usiamo l'operatore di assegnamento nella dichiarazione delle variabili
     int num1=5,num2=15,risultato;
     printf("Le variabili sono le seguenti:\n");
     printf("TIPO\t\t\tNOME\t\t\tVALORE\n");
@@ -121,36 +78,49 @@ int main()
     printf("\n=   ");
     showbits(risultato);
 
+
     //uso degli operatori aritmetici
-    printf("\n\n\nOperazioni con gli operatori aritmetici:\n");
-    int sum=num1+num2;
-    int sub=num1-num2;
-    int prod=num1*num2;
-    float div=(float)num1/num2; //conversione esplicita da int a float usando il casting
-    int mod=num1%num2;
+    printf("\nOperazioni con gli operatori aritmetici:\n");
+    int sum = num1 + num2;
+    int sub = num1 - num2;
+    int prod = num1 * num2;
+    float div =(float)num1 / num2; //conversione esplicita da int a float usando il casting
+    int mod = num1 % num2;
     printf("Somma:\t\t\t%3d\n",sum);
     printf("Sottrazione:\t\t%d\n",sub);
     printf("Prodotto:\t\t%3d\n",prod);
     printf("Divisione:\t\t%f\n",div);
-    printf("Modulo:\t\t\t%3d\n",mod);
+    printf("Modulo:\t\t\t%3d\n\n",mod);
+
     //da qui usiamo operatori  di  assegnamento  compatti per modificare il valore di num1
-    printf("\tnum1 iniziale =\t%3d\n",num1);
-    printf("num1=num1(%d)+num2(%d)\t",num1, num2);
+    printf("num1 iniziale =\t%3d\n\n",num1);
+    printf("num1 = num1(%d) + num2(%d)\t",num1, num2);
     num1+=num2;
-    printf("num1=%3d\n",num1);
-    printf("num1=num1(%d)-num2(%d)\t",num1, num2);
+    printf("num1 = %3d\n",num1);
+    printf("num1 = num1(%d) - num2(%d)\t",num1, num2);
     num1-=num2;
-    printf("num1=%3d\n",num1);
-    printf("num1=num1(%d)*num2(%d)\t",num1, num2);
+    printf("num1 = %3d\n",num1);
+    printf("num1 = num1(%d) * num2(%d)\t",num1, num2);
     num1*=num2;
-    printf("num1=%3d\n",num1);
-    printf("num1=num1(%d)/num2(%d)\t",num1, num2);
+    printf("num1 = %3d\n",num1);
+    printf("num1 = num1(%d) / num2(%d)\t",num1, num2);
     num1/=num2;
-    printf("num1=%3d\n",num1);
-    printf("num1=num1(%d)%%num2(%d)\t",num1, num2);
+    printf("num1 = %3d\n",num1);
+    printf("num1 = num1(%d) % num2(%d)\t",num1, num2);
     num1%=num2;
-    printf("num1=%3d\n",num1);
-    printf("\tnum1 finale =\t%3d\n",num1);
+    printf("num1 = %3d\n\n",num1);
+    printf("num1 finale =\t%3d\n",num1);
+
+    //Effetti collaterali
+    int x = 1;
+
+    x = x++;
+    printf("\nIl valore di x con side effect e\' %d\n\n",x);
+    x = 1;
+    x = ++x;
+    printf("Il valore di x e\' %d\n\n",x);
+
+
     //qui usiamo gli operatori aritmetici unari, incremento e decrementano il valore di una variabile
     printf("\nvalori iniziali delle variabili:  %d  %d\n", num1, num2);
     printf("incrementi e decrementi:\t  %d  %d\n", num1++, --num2);//num1 invariato perchè assume il valore prima dell'incremento
@@ -159,13 +129,9 @@ int main()
     printf("\t\t\t\t  %d  %d\n", num1++, --num2);
     printf("valori finali:\t\t\t  %d  %d\n", ++num1, --num2);
     printf("valori finali cambiati di segno: %d %d\n", -num1, -num2);
-    /* FUNZIONI MATEMATICHE math.h nei lucidi 4 p. 137
-    floor(x)
-    ceil(x)
-    fabs(x)
-    ....
-    ne dobbiamo fare almeno 15 (mai na gioia)
-    */
+
+    //qui usiamo le funzione matematiche
+
     printf("\nFunzioni matematiche per il num1(%d)\n", num1);
     printf("1. Radice quadrata:\t\t\t%lf\n", sqrt(num1));
     printf("2. e elevato alla num1:\t\t\t%lf\n", exp(num1));
@@ -180,8 +146,6 @@ int main()
     printf("9. Arc coseno in radianti:\t\t%lf\n",  acos(num6));
     printf("10. Arc seno in radianti:\t\t%lf\n",  asin(num6));
     printf("11. Arc tangente in radianti:\t\t%lf\n",  atan(num6));
-    /*double atan2(double y, double x)
-    Returns the arc tangent in radians of y/x based on the signs of both values to determine the correct quadrant.*/
     printf("12. Coseno iperbolico in radianti:\t%lf\n",  cosh(num6));
     printf("13. Seno iperbolico in radianti:\t%lf\n",  sinh(num6));
     printf("14. Tangente iperbolica in radianti:\t%lf\n",  tanh(num6));
@@ -189,98 +153,97 @@ int main()
     printf("16. Maggior valore intero <= num6:\t%lf\n",  floor(num6));
 
 
+
     //operatori logici e relazionali applicati con l'istruzione di selezione if
-    int a, b, c, num3, num4, num5, flag;
-    do
+    int num3,num4,num5;
+
+    printf("Dammi il primo valore ");
+    scanf("%d",&num3);
+    fflush(stdin);
+    printf("Dammi il secondo valore ");
+    scanf("%d",&num4);
+    fflush(stdin);
+    printf("Dammi il terzo valore ");
+    scanf("%d",&num5);
+    fflush(stdin);
+
+    printf("Avviamo il confronto");
+    if(num3 != num4 && num4!= num5 && num3 != num5)
     {
-        printf("\nInserisci 3 numeri interi (int) da confrontare:\na:\t");
-        scanf("%d", &a);
-        fflush(stdin);
-        printf("b:\t");
-        scanf("%d", &b);
-        fflush(stdin);
-        printf("c:\t");
-        scanf("%d", &c);
-        fflush(stdin);
-        if (a!=b&&b!=c&&a!=c)
+        printf("I numeri sono tutti diversi tra loro, stampati in ordine crescente risultano ");
+        if(num3 < num4 && num4 < num5)
         {
-            printf("I numeri sono tutti diversi tra loro.\t");
-            if (a>b)
-            {
-                if(b>c)
-                {
-                    num3 = c;
-                    num4 = b;
-                    num5 = a;
-                }
-                else
-                {
-                    num3=b;
-                    if (c>a)
-                    {
-                        num4 = a;
-                        num5 = c;
-                    }
-                    else
-                    {
-                        num4 = c;
-                        num5 = a;
-                    }
-                }
-            }
-            else
-            {
-                if (a>c)
-                {
-                    num3=c;
-                    num4=a;
-                    num5=b;
-                }
-                else
-                {
-                    num3=a;
-                    if (c>b)
-                    {
-                        num4=b;
-                        num5=c;
-                    }
-                    else
-                    {
-                        num4=c;
-                        num5=b;
-                    }
-                }
-            }
-            printf("Nell'ordine: %d < %d < %d.", num3, num4, num5);
+            printf("%d<%d<%d",num3,num4,num5);
+        }
+        else if(num3 < num5 && num5 < num4)
+        {
+            printf("%d<%d<%d",num3,num5,num4);
+        }
+        else if(num4 < num3 && num3 < num5)
+        {
+            printf("%d<%d<%d",num4,num3,num5);
+        }
+        else if(num4 < num5 && num5 < num3)
+        {
+            printf("%d<%d<%d",num4,num5,num3);
+        }
+        else if(num5 < num4 && num4 < num3)
+        {
+            printf("%d<%d<%d",num5,num4,num3);
         }
         else
         {
-            printf("Numeri uguali tra loro:\n");
-            if (a==b)
+            printf("%d<%d<%d",num5,num3,num4);
+        }
+    }
+    else if(num3 == num4 && num4 == num5)
+    {
+        printf("I numeri inseriti sono tutti uguali fra loro");
+    }
+    else
+    {
+        printf("Almeno 2 valori sono uguali tra loro");
+        if(num3 == num4)
+        {
+            if(num3 > num5)
             {
-                printf("a(%d)=b(%d)", a, b);
+                printf("num3 e num4 sono > di num5, %d e %d > %d",num3,num4,num5);
             }
-            if (b==c)
+            else
             {
-                printf("b(%d)=c(%d)", b, c);
-            }
-            if (a==c)
-            {
-                printf("a(%d)=c(%d)", a, c);
+                printf("num3 e num4 sono < di num5, %d e %d < %d",num3,num4,num5);
             }
         }
-        printf("\n\nPer smettere di confrontare numeri, digitare 1:\t");
-        scanf("%d", &flag);
-        fflush(stdin);
+        else if(num4 == num5)
+        {
+             if(num4 > num3)
+            {
+                printf("num4 e num5 sono > di num3, %d e %d > %d",num4,num5,num3);
+            }
+            else
+            {
+                printf("num4 e num5 sono < di num3, %d e %d < %d",num4,num5,num3);
+            }
+        }
+        else
+        {
+             if(num5 > num4)
+            {
+                printf("num3 e num5 sono > di num4, %d e %d > %d",num3,num5,num4);
+            }
+            else
+            {
+                printf("num3 e num5 sono < di num4, %d e %d < %d",num3,num5,num3);
+            }
+        }
     }
-    while (flag!=1);
 
-    printf("\n\n\n\n");
+
     system("pause");
     return 0;
 }
 
-void showbits(unsigned int x )
+void showbits( unsigned int x )
 {
     int i=0;
     for (i = (sizeof(int) * 8) - 1; i >= 0; i--)
