89c65f3e4c9514606013a97b6dfba284b5be037a
Sara Romeo <57600591+sara-romeo@users.noreply.github.com>
2020-10-15 10:20:18 +0200

    Add files via upload

---
+++ Consegna-3/20201021_Elaborato01.c
@@ -0,0 +1,21 @@
+/*AUTHOR: Cristina Zoccola(cristina.zoccola@studio.unibo.it)
+          Denny Reggidori(denny.reggidori@studio.unibo.it)
+          Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
+          Sara Romeo(sara.romeo3@studio.unibo.it)
+ DATE:    20201007
+ NOTE:    Text Ex01: Scrivere un programma C che legga da tastiera un testo terminato dal carattere #
+          e poi stampi le statistiche relative al numero di spazi, al numero di caratteri di nuova linea,
+          al numero di caratteri letti, al numero di quelli maiuscoli, etc.
+          utilizzando almeno una volta tutte le funzioni dell’header ctype.h*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+
+    printf("\n\n\n\n");
+    system("pause");
+    return 0;
+}
---
+++ Consegna-3/20201021_Elaborato02.c
@@ -0,0 +1,19 @@
+/*AUTHOR: Cristina Zoccola(cristina.zoccola@studio.unibo.it)
+          Denny Reggidori(denny.reggidori@studio.unibo.it)
+          Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
+          Sara Romeo(sara.romeo3@studio.unibo.it)
+ DATE:    20201007
+ NOTE:    Text Ex02: Scrivere un programma C che stampi il calendario di un anno,
+          chiedendo all'utente di specificare l'anno in oggetto e il numero che identifica il primo giorno dell'anno
+          (1=lunedì, 7=domenica).*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("\n\n\n\n");
+    system("pause");
+    return 0;
+}
