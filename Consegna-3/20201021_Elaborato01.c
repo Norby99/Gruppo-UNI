d97c85a84ef1baf79f1a931957c436a6d2d06562
Sara Romeo <57600591+sara-romeo@users.noreply.github.com>
2020-10-15 16:54:11 +0200

    Update 20201021_Elaborato01.c

--- Consegna-3/20201021_Elaborato01.c
+++ Consegna-3/20201021_Elaborato01.c
@@ -8,15 +8,30 @@
           al numero di caratteri letti, al numero di quelli maiuscoli, etc.
           utilizzando almeno una volta tutte le funzioni dell’header ctype.h*/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
 
-int main()
+#define CARATTERE '#'
+
+int main ()
 {
     //link piccinini https://www.programiz.com/c-programming/library-function/ctype.h
+    
+    //Inserimento di una stringa che termina con #
+    printf("Inserire un testo da terminare col carattere #:\n");
+    char ch;
+    char bufferA[100];
+    int i, index = 0;
+    memset(bufferA, '\0', sizeof(bufferA)); /*function memset() used to clear the memory location*/
+    while ((ch = getchar()) != CARATTERE){  //legge un singolo carattere, restituendolo
+        bufferA[index++]=ch;
+    }
+    printf("The input sentence is:\n%s\n", bufferA);
+    //uso le funzioni della libreria <ctype.h>
+    printf("Number of white spaces: %d\n", isspace(bufferA));
 
-    printf("\n\n\n\n");
-    system("pause");
+    system ("pause");
     return 0;
 }
