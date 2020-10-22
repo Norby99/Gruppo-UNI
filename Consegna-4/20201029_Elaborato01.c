b081a2cd9bef3898dc3298e1bd003d5ad158df92
Sara Romeo <57600591+sara-romeo@users.noreply.github.com>
2020-10-22 17:33:06 +0200

    Update 20201029_Elaborato01.c

--- Consegna-4/20201029_Elaborato01.c
+++ Consegna-4/20201029_Elaborato01.c
@@ -15,7 +15,53 @@
 int main()
 {
     //
+    int i, n1, n2, n3;
 
+    printf("\"Intersection between 2 arrays\"\nEnter array 1 length: ");
+    scanf("%d", &n1);
+    fflush(stdin);
+    printf("Fill the array:\n");
+    int a1[n1];
+    for (i=0; i<n1; i++)
+    {
+        printf("%d: ", i); //I'd like to put i++ here to order from 1 to 10 the output, but it messes the code up
+        scanf("%d", &a1[i]);
+        fflush(stdin);
+    }
+    //do-while loop to check there's a different length in input
+    do
+    {
+        printf("\nEnter array 2 length: ");
+        scanf("%d", &n2);
+        fflush(stdin);
+        if (n2==n1)
+        {
+            printf("\n!! Value must be different from the previous one.\n");
+        }
+    }
+    while (n2==n1);
+    printf("Fill the array:\n");
+    int a2[n2];
+    for (i=0; i<n2; i++)
+    {
+        printf("%d: ", i); //I'd like to put i++ here to order from 1 to 10 the output, but it messes the code up
+        scanf("%d", &a2[i]);
+        fflush(stdin);
+    }
+    //
+
+
+
+
+    int a3[n3];
+
+
+    // output of the array
+    printf("\nHere's the full array:\n");
+    for (i=0; i<n3; i++)
+        printf("%d ", a3[i]);
+
+    printf("\n\n\n\n");
     system ("pause");
     return 0;
 }
