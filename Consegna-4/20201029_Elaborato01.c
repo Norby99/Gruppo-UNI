8d892971cc30204df55ae201c4b2da335a1e1dd8
Sara Romeo <57600591+sara-romeo@users.noreply.github.com>
2020-10-22 11:30:56 +0200

    Add files via upload

---
+++ Consegna-4/20201029_Elaborato01.c
@@ -0,0 +1,21 @@
+/*AUTHOR: Cristina Zoccola(cristina.zoccola@studio.unibo.it)
+          Denny Reggidori(denny.reggidori@studio.unibo.it)
+          Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
+          Sara Romeo(sara.romeo3@studio.unibo.it)
+ DATE:    20201022
+ NOTE:    Text Ex01: Siano dati due vettori di interi di lunghezza differente i cui valori sono inseriti da tastiera.
+          Scrivere un programma C che generi un terzo vettore contenente l’intersezione tra i due vettori di input.
+          Se un elemento è comune ai due vettori e presente più volte in un vettore farlo comparire anche nel vettore risultato.
+          Implementare la variante in cui le ripetizioni non sono presenti nel risultato.
+          Implementare inoltre anche l'inserimento random dei valori nei vettori.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    //
+
+    system ("pause");
+    return 0;
+}
---
+++ Consegna-4/20201029_Elaborato02.c
@@ -0,0 +1,22 @@
+/*AUTHOR: Cristina Zoccola(cristina.zoccola@studio.unibo.it)
+          Denny Reggidori(denny.reggidori@studio.unibo.it)
+          Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
+          Sara Romeo(sara.romeo3@studio.unibo.it)
+ DATE:    20201022
+ NOTE:    Text Ex02: Scrivere un programma C che legga N numeri interi da tastiera e li memorizzi in un vettore.
+          Il programma deve generare un secondo vettore compattando i numeri contenuti nel primo vettore. In particolare:
+            1. ogni numero che compare ripetuto nel primo vettore, deve comparire una sola volta nel secondo vettore
+            2. ogni numero uguale a zero presente nel primo vettore non deve comparire nel secondo vettore
+          Prevedere anche la versione in cui i numeri sono generati random.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    //
+
+    system("pause");
+    return 0;
+}
