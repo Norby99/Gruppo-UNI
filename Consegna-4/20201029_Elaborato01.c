f99bebf7173af670d3629abb81697eb19348dc3b
Norby99 <72309039+Norby99@users.noreply.github.com>
2020-10-23 18:41:51 +0200

    Update 20201029_Elaborato01.c

--- Consegna-4/20201029_Elaborato01.c
+++ Consegna-4/20201029_Elaborato01.c
@@ -10,8 +10,8 @@
           Implementare inoltre anche l'inserimento random dei valori nei vettori.*/
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define RANDOMNUMBER 5
@@ -21,8 +21,12 @@ int *randomisedArray(int);
 int main(void)
 {
     // entering array values
-    int i, n1=0, n2=0, n3=0, a1[n1], a2[n2];
+    int i, n1, n2, n3;
+    int *a1, *a2, *a3, *a4;
     char inputType;
+    int j, n4=0;
+    bool flag=false, fl=false, f=false;
+
     printf("\"Intersection between 2 arrays\"");
     do
     {
@@ -34,95 +38,107 @@ int main(void)
         else
         {
            do
-           {
-               printf("\n\nEnter array 1 length: ");
-               scanf("%d", &n1);
-               fflush(stdin);
-               if (n1<=0)
-               {
-                   printf("!! The length format is invalid. Please put a number >0.\n");
-                   continue;
-               }
-               else if (inputType=='m')
-               {
-                   printf("Fill the array:\n");
-                   for (i=0; i<n1; i++)
-                   {
-                       printf("%d: ", i); //I'd like to put i++ here to order from 1 to 10 the output, but it messes the code up
-                       scanf("%d", &a1[i]);
-                       fflush(stdin);
-                   }
-               }
-               /*else if(inputType == 'r'){
-                    a1 = randomisedArray(n1);
-               }*/
-               // do-while loop to check if there's a different input length besides checking n2 correct length value
-               do
-               {
-                   printf("\nEnter array 2 length: ");
-                   scanf("%d", &n2);
-                   fflush(stdin);
-                   if (n2<=0)
-                   {
-                       printf("!! The length format is invalid. Please put a number >0.\n");
-                       continue;
-                   }
-                   if (n2==n1)
-                   {
-                       printf("\n!! Value must be different from the previous one.\n");
-                   } else if (inputType=='m')
-                   {
-                       printf("Fill the array:\n");
-                       for (i=0; i<n2; i++)
-                       {
-                           printf("%d: ", i); //I'd like to put i++ here to order from 1 to 10 the output, but it messes the code up
-                           scanf("%d", &a2[i]);
-                           fflush(stdin);
-                       }
-                   } /*else if(inputType == 'r'){
+            {
+                printf("\n\nEnter array 1 length: ");
+                scanf(" %d", &n1);
+                a1 = malloc(sizeof(int) * n1);
+
+                if (n1<=0)
+                {
+                    printf("!! The length format is invalid. Please put a number >0.\n");
+                    continue;
+                }
+                else if (inputType=='m')
+                {
+                    printf("Fill the array:\n");
+                    for (i=0; i<n1; i++)
+                    {
+                        printf("%d: ", i); //I'd like to put i++ here to order from 1 to 10 the output, but it messes the code up
+                        scanf(" %d", &a1[i]);
+                    }
+                }
+                else if(inputType == 'r'){
+                        a1 = randomisedArray(n1);
+                }
+                // do-while loop to check if there's a different input length besides checking n2 correct length value
+                do
+                {
+                    printf("\nEnter array 2 length: ");
+                    scanf("%d", &n2);
+                    a2 = malloc(sizeof(int) * n2);
+
+                    if (n2<=0)
+                    {
+                        printf("!! The length format is invalid. Please put a number >0.\n");
+                        continue;
+                    }
+                    if (n2==n1)
+                    {
+                        printf("\n!! Value must be different from the previous one.\n");
+                    } else if (inputType=='m')
+                    {
+                        printf("Fill the array:\n");
+                        for (i=0; i<n2; i++)
+                        {
+                            printf("%d: ", i); //I'd like to put i++ here to order from 1 to 10 the output, but it messes the code up
+                            scanf(" %d", &a2[i]);
+                        }
+                    }else if(inputType == 'r'){
                         a2 = randomisedArray(n2);
-                   }*/
-               } while (n2==n1||n2<=0);
-           } while(n1<=0);
-         }
+                }
+                } while (n2==n1||n2<=0);
+            } while(n1<=0);
+        }
     } while(inputType != 'm' && inputType != 'r');
+
     // finding intersections and storing them in a 3rd array
-    int j, a3[n3], n4=0, a4[n4];
-    bool flag=0, fl=0, f=0;
+    a3 = malloc(sizeof(int) * n1);
+    a4 = malloc(sizeof(int) * n1);
     for(i=0; i<n1; i++)
     {
         for(j=0; j<n2; j++)
         {
-            if(a2[i] == a1[j])
+            if(a2[j] == a1[i])
             {
-                flag = 1;
-                fl=1;
+                flag = true;
+                fl=true;
+                break;
             }
         }
-        if(flag == 1)
+        if(flag == true)
         {
-            a3[n3]=a2[i];
-            f=0;
+            a3[n3]=a2[j];
+
+            f=false;
             for (j=0; j<n3; j++)
             {
                 if (a4[j]==a3[n3])
                 {
-                    f=1;
+                    f=true;
+                    break;
                 }
             }
-            if (f==0)
+            if (f==false)
             {
                 a4[n4]=a3[n3];
                 n4++;
             }
             n3++;
         }
-        flag = 0;
+        flag = false;
     }
 
-
     // output of the final array
-    if (fl==0)
+
+    if(inputType == 'r'){
+        printf("\nArray 1:\n");
+        for (i=0; i<n1; i++)
+            printf("%d ", a1[i]);
+        printf("\nArray 2:\n");
+        for (i=0; i<n2; i++)
+            printf("%d ", a2[i]);
+    }
+    if (fl==false)
         printf("\nThere's no intersection between the 2 arrays.\n");
     else
     {
@@ -135,17 +151,16 @@ int main(void)
     }
 
     printf("\n\n\n\n");
-    system ("pause");
+    system("pause");
     return 0;
 }
 // external randomize function
-int *randomisedArray(int length)
-{
+int *randomisedArray(int length){
     int *array = malloc(length * sizeof(*array));
     srand(time(NULL));
-    // filling the array with integer random numbers
+
     for(int i=0; i<length; i++){
-        array[i] = -(rand() % RANDOMNUMBER)+(rand() % RANDOMNUMBER);
+        array[i] = (rand() % RANDOMNUMBER);
     }
     return array;
 }
