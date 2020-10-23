a6569565232e30e5935cd11a87f05a6b5979ee80
Sara Romeo <57600591+sara-romeo@users.noreply.github.com>
2020-10-23 19:55:30 +0200

    Update 20201029_Elaborato01.c

--- Consegna-4/20201029_Elaborato01.c
+++ Consegna-4/20201029_Elaborato01.c
@@ -2,7 +2,7 @@
           Denny Reggidori(denny.reggidori@studio.unibo.it)
           Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
           Sara Romeo(sara.romeo3@studio.unibo.it)
- DATE:    20201022
+ DATE:    20201023
  NOTE:    Text Ex01: Siano dati due vettori di interi di lunghezza differente i cui valori sono inseriti da tastiera.
           Scrivere un programma C che generi un terzo vettore contenente l’intersezione tra i due vettori di input.
           Se un elemento è comune ai due vettori e presente più volte in un vettore farlo comparire anche nel vettore risultato.
