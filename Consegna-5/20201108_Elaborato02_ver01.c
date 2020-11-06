e425db0f7d9625040d7b5e72e0b4c5a8871c1bb9
DennyReggidori <72309051+DennyReggidori@users.noreply.github.com>
2020-11-06 16:16:20 +0100

    Update 20201108_Elaborato02_ver01.c

--- Consegna-5/20201108_Elaborato02_ver01.c
+++ Consegna-5/20201108_Elaborato02_ver01.c
@@ -2,7 +2,9 @@
           Denny Reggidori(denny.reggidori@studio.unibo.it)
           Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
           Sara Romeo(sara.romeo3@studio.unibo.it)
+
  DATE:    20201030
+
  NOTE:    Text Ex02: Scrivere un programma C che riceva in ingresso due parole inserite da tastiera.
           Si consideri che ciascuna parola può contenere al massimo 30 caratteri. Il programma deve essere case sensitive,
           cioè deve distinguere lettere minuscole da lettere maiuscole e deve essere in grado anche di analizzare numeri,
@@ -21,20 +23,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define MAX_LENGTH 30
 
 int string_length (char *);
 void getString (char *);
 void strReplace(char *, char *, char);
+char *str_replace(char *, char *, char *);
 
 int main (void)
 {
     char *s1, *s2;
+    int i, scelta;
 
     s1=malloc(sizeof(char)*100);
     s2=malloc(sizeof(char)*100);
 
+    do
+    {
+        printf("What version do you want to run, press 1(with string.h) or 2(without string.h): ");
+        scanf("%d",&scelta);
+    }while(scelta != 1 && scelta != 2);
+
+    printf("\n");
+    if(scelta==2)
+    {
     printf("Enter a word, max 30 characters: ");
     getString(s1);
 
@@ -43,7 +57,25 @@ int main (void)
 
     strReplace(s1, s2, '*');
 
-    printf ("Replacement:\n%s", s1);
+    printf ("\nReplacement:\n%s\n", s1);
+    }
+    else
+    {
+        printf("Enter a word, max 30 characters: ");
+        s1=malloc(sizeof(char)*100);
+        getString(s1);
+        printf ("Word: %s\n\n", s1);
+        printf("Enter a second word, max 30 characters: ");
+        s2=malloc(sizeof(char)*100);
+        getString(s2);
+        printf ("Characters to replace with *: %s\n\n", s2);
+
+        printf ("Substitute given characters in word with *:\n%s\n", str_replace(s1, s2, "*"));
+
+
+        printf ("\n\n");
+        system ("pause");
+    }
 
     return 0;
 }
@@ -93,3 +125,51 @@ void strReplace (char *s1, char *s2, char val)
         }
     }
 }
+
+char *str_replace (char *orig, char *rep, char *with) {
+    char *result; // the return string
+    char *ins;    // the next insert point
+    char *tmp;    // varies
+    int len_rep;  // length of rep (the string to remove)
+    int len_with; // length of with (the string to replace rep with)
+    int len_front;// distance between rep and end of last rep
+    int count;    // number of replacements
+
+    // sanity checks and initialization
+    if (!orig || !rep)
+        return NULL;
+    len_rep = strlen(rep);
+    if (len_rep == 0)
+        return NULL; // empty rep causes infinite loop during count
+    if (!with)
+        with = "";
+    len_with = strlen(with);
+
+    // count the number of replacements needed
+    ins = orig;
+    for (count = 0; tmp = strstr(ins, rep); ++count) {
+        ins = tmp + len_rep;
+    }
+
+    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+
+    if (!result)
+        return NULL;
+
+    // first time through the loop, all the variable are set correctly
+    // from here on,
+    //    tmp points to the end of the result string
+    //    ins points to the next occurrence of rep in orig
+    //    orig points to the remainder of orig after "end of rep"
+    while (count--) {
+        ins = strstr(orig, rep);
+        len_front = ins - orig;
+        tmp = strncpy(tmp, orig, len_front) + len_front;
+        tmp = strcpy(tmp, with) + len_with;
+        orig += len_front + len_rep; // move to next "end of rep"
+    }
+    strcpy(tmp, orig);
+    return result;
+}
+
+
