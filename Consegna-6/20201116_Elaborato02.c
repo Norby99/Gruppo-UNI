86c4a7224b9f5f40fffcb8326d75eaef2c8ab031
DennyReggidori <72309051+DennyReggidori@users.noreply.github.com>
2020-11-12 16:34:03 +0100

    Update 20201116_Elaborato02.c

--- Consegna-6/20201116_Elaborato02.c
+++ Consegna-6/20201116_Elaborato02.c
@@ -14,10 +14,124 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <float.h>
+#include <ctype.h>
+#define MAX 5
 
 int main()
 {
+    int num_Intero, vet[MAX]= {10, 20, 30 ,40, 50},i;
+    char carattere;
+    float num_RealeF;
+    double num_RealeD;
+
+    int *p_num_Intero=&num_Intero, *p_vet=vet;
+    char *p_carattere=&carattere;
+    float *p_num_RealeF=&num_RealeF;
+    double *p_num_RealeD=&num_RealeD;
+
+    printf("Inserisci un numero intero: ");
+
+    while( scanf("%d",&num_Intero)!=1)
+    {
+       printf("\nIl valore inserito non e\' conforme al tipo di varibile , riprova ");
+       scanf("%d",&num_Intero);
+       fflush(stdin);
+    }
+    printf("\n");
+    fflush(stdin);
+
+    printf("Inserisci un carattere: ");
+    scanf("%c",&carattere);
+    fflush(stdin);
+
+    while(isdigit(carattere)==1)
+    {
+        printf("\nIn realta\' il carattere inserito e\' una cifra, dovresti inserire una lettera, riprova:  ");
+        scanf("%c",&carattere);
+        fflush(stdin);
+    }
+    printf("\n");
+    fflush(stdin);
+
+    printf("Inserisci un numero reale(float): ");
+
+    while(scanf("%f",&num_RealeF)!=1)
+    {
+        printf("\nIl valore inserito non e\' conforme al tipo di varibile , riprova ");
+        scanf("%f",&num_RealeF);
+        fflush(stdin);
+    }
+    printf("\n");
+    fflush(stdin);
+
+    printf("Inserisci un numero reale(double):");
+
+    while(scanf("%lf",&num_RealeD)!=1)
+    {
+       printf("\nIl valore inserito non e\' conforme al tipo di varibile , riprova ");
+       scanf("%f",&num_RealeD);
+       fflush(stdin);
+    }
+    printf("\n");
+    fflush(stdin);
+
+    printf("\nACCESSO DIRETTO\n");
+    printf("Numero Intero: %d\n",num_Intero);
+    printf("Carattere: %c\n",carattere);
+    printf("Numero Reale(float): %.2f\n",num_RealeF);
+    printf("Numero Reale(double): %.6lf\n",num_RealeD);
+    printf("Vettore: ");
+    for(i=0;i<MAX;i++)
+    {
+        printf("%d ",vet[i]);
+    }
+
+
+    printf("\n\nACCESSO INDIRETTO\n");
+    printf("Numero Intero: %d\n",*p_num_Intero);
+    printf("Carattere: %c\n",*p_carattere);
+    printf("Numero Reale(float): %.2f\n",*p_num_RealeF);
+    printf("Numero Reale(double): %.6lf\n",*p_num_RealeD);
+    printf("Vettore: ");//IL VETTORE È DA RIVEDERE
+    for(i=0;i<MAX;i++)
+    {
+        printf("%d ",*p_vet++);
+    }
+
+    printf("\n\nINDIRIZZI DELLE VARIABILI(Diretto)\n");
+    printf("Numero Intero: %d\n",&num_Intero);
+    printf("Carattere: %d\n",&carattere);
+    printf("Numero Reale(float): %d\n",&num_RealeF);
+    printf("Numero Reale(double): %d\n",&num_RealeD);
+    printf("Vettore: ");
+    for(i=0;i<MAX;i++)
+    {
+        printf("%d ",&vet[i]);
+    }
+
+    printf("\n\nINDIRIZZI DELLE VARIABILI(Indiretto)\n");
+    printf("Numero Intero: %d\n",p_num_Intero);
+    printf("Carattere: %d\n",p_carattere);
+    printf("Numero Reale(float): %d\n",p_num_RealeF);
+    printf("Numero Reale(double): %d\n",p_num_RealeD);
+    printf("Vettore: ");//IL VETTORE È DA RIVEDERE
+    for(i=0;i<MAX;i++)
+    {
+        printf("%d ",p_vet++);
+    }
+
+    printf("\n\nSTAMPA DEGLI INDIRIZZI DI MEMORIA DEI PUNTAORI STESSI\n");
+     printf("Numero Intero: %d\n",&p_num_Intero);
+    printf("Carattere: %d\n",&p_carattere);
+    printf("Numero Reale(float): %d\n",&p_num_RealeF);
+    printf("Numero Reale(double): %d\n",&p_num_RealeD);
+    printf("Vettore: "); //IL VETTORE È DA RIVEDERE
+    for(i=0;i<MAX;i++)
+    {
+        printf("%d ",&p_vet);
+    }
 
     system("pause");
-	return 0;
+    return 0;
 }
