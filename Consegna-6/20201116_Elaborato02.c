efa315b4975c7dd79f0f97c492ffe172070185f2
Sara Romeo <57600591+sara-romeo@users.noreply.github.com>
2020-11-12 15:10:01 +0100

    Add files via upload

---
+++ Consegna-6/20201116_Elaborato02.c
@@ -0,0 +1,23 @@
+/*AUTHOR: Cristina Zoccola(cristina.zoccola@studio.unibo.it)
+          Denny Reggidori(denny.reggidori@studio.unibo.it)
+          Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
+          Sara Romeo(sara.romeo3@studio.unibo.it)
+ DATE:    20201113
+ NOTE:    Text Ex02: Scrivere un programma C dove sono inizializzate una variabile di tipo int, una di tipo float, una di tipo double,
+          una di tipo char, ed un array di 5 interi (precisamente con i valori 10, 20, 30, 40, 50).
+          Inizializzare un puntatore per ciascuna di queste variabili. Assegnare ad ogni variabile un valore tramite
+          l'utilizzo della funzione scanf. Visualizzare il valore di ogni variabile in due modi:
+          prima tramite accesso diretto alla variabile e a seguito tramite l'utilizzo del suo puntatore (accesso indiretto).
+          Visualizzare la posizione in memoria della variabile tramite l'utilizzo diretto della variabile e tramite il suo puntatore.
+          Visualizzare la posizione in memoria del puntatore.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    system("pause");
+	return 0;
+}
