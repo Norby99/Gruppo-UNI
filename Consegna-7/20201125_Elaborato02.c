d4b8c05bfd61f5919053fe44f596553c7b2ac82c
Sara Romeo <57600591+sara-romeo@users.noreply.github.com>
2020-11-18 20:29:26 +0100

    Add files via upload

---
+++ Consegna-7/20201125_Elaborato02.c
@@ -0,0 +1,28 @@
+/*AUTHOR: Cristina Zoccola(cristina.zoccola@studio.unibo.it)
+          Denny Reggidori(denny.reggidori@studio.unibo.it)
+          Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
+          Sara Romeo(sara.romeo3@studio.unibo.it)
+ DATE:    20201120
+ NOTE:    Text Ex02: Si scriva un programma C, adeguatamente commentato, che gestisca un certo numero di studenti iscritti ad un corso di laurea. Il programma utilizza un vettore di strutture, ognuna delle quali memorizza le informazioni di uno studente (numero matricola, nome e cognome, anno di immatricolazione, piano di studi, …). Il programma utilizza inoltre un vettore di strutture, ognuna delle quali memorizza le informazioni di un insegnamento (codice, descrizione, anno di somministrazione, crediti, …).
+          Il programma deve fornire la possibilità di:
+          - aggiungere un nuovo studente, stampando un messaggio di errore se lo studente è già presente (individuato dalla matricola)
+          o se il vettore è pieno; la funzione deve inoltre assegnargli un piano di studi scegliendo fra gli insegnamenti disponibili.
+          - dato il numero di matricola, stampare le informazioni dello studente oppure un messaggio di errore
+          se la matricola non è presente nel vettore
+          - dato un numero di matricola, modificare le informazioni del piano di studi aggiungendo un voto
+          oppure stampando un messaggio di errore se la matricola non è presente nel vettore.
+          - Calcolare la media dei voti di tutti gli studenti che hanno completato il piano di studi pesata sul numero di crediti
+          di ogni insegnamento.
+          Il piano di studi all’interno della struttura studente è un vettore di NUM_ESAMI elementi ognuno di tipo struttura
+          contenete il codice dell'insegnamento e il voto corrispondente (0 se non sostenuto).
+          Modificare poi il programma in modo che il vettore di studenti venga allocato dinamicamente usando la funzione malloc().*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+    // 2 versioniiiiiiiiiiiiiiiiiiiiiiiii :))))))))))
+    system("pause");
+    return 0;
+}
