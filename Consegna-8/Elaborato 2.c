0489621da241eb045f18c71ad218c285ad187ae9
Sara Romeo <57600591+sara-romeo@users.noreply.github.com>
2020-12-04 17:25:47 +0100

    Update Elaborato 2.c

--- Consegna-8/Elaborato 2.c
+++ Consegna-8/Elaborato 2.c
@@ -1,3 +1,16 @@
+/*AUTHOR: Cristina Zoccola(cristina.zoccola@studio.unibo.it)
+          Denny Reggidori(denny.reggidori@studio.unibo.it)
+          Norby Gabos(tiberiunorbert.gabos@studio.unibo.it)
+          Sara Romeo(sara.romeo3@studio.unibo.it)
+ DATE:    20201204
+ NOTE:    Text Ex02: Realizzare un programma che risolva tramite una funzione ricorsiva un qualche problema scelto dagli studenti;
+          il programma deve stampare le variabili locali e i parametri di ogni chiamata alla funzione ricorsiva.
+          In particolare, per ogni chiamata ricorsiva, deve visualizzare l’output su una riga separata e aggiungere un livello di
+          rientro. Fate del vostro meglio per rendere l’output chiaro, interessante e significativo.
+          L’obiettivo è quello di progettare e implementare un formato dell’output che aiuti l'utente a comprendere il meccanismo
+          della ricorsione.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,21 +22,21 @@ typedef struct{
     int len;
 }myArray;
 
-
-// ? Funzioni generiche
+// Funzioni generiche
 void LookAndSay(myArray, int);
 
 
-// ? Metodi di myArray
+// Metodi di myArray
 myArray newMyArray();
 void printMyArray(myArray);
 void appendMyArray(myArray*, int);
 void destroyer(myArray*);
 
-
 int main()
 {
-    printf("Questa programma genera la sequenza di Look And say,\ndove abbiamo la prima riga che e' una costante (ovvero 1)\ne le successive si generano in base al contenuto di quella precedente.\nIl procedimento e' il seguente:\nSi individua il primo numero e si fa il conteggio di quante volte compare\ndi fila e si printail numero ottenuto e il numero stesso e cosi' via\n\n");
+    printf("\n\t\t***** LOOK AND SAY *****\n\nQuesto programma genera una sequenza di Look and Say:\nla prima riga e' una costante (ovvero 1),");
+    printf("\nle successive si generano in base al contenuto di quella precedente.\nIl procedimento e' il seguente:");
+    printf("\nSi individua il primo numero e si fa il conteggio di quante volte compare\ndi fila e si printa il numero ottenuto e il numero stesso e cosi' via\n\n");
     myArray array = newMyArray();
     appendMyArray(&array, 1);
 
