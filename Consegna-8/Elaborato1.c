88897f24faa5ca454bf4d12f8c207b9843931654
DennyReggidori <72309051+DennyReggidori@users.noreply.github.com>
2020-12-04 15:54:05 +0100

    Update Elaborato1.c

--- Consegna-8/Elaborato1.c
+++ Consegna-8/Elaborato1.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 #define MAX_LENGTH 500
 
 void checkEachWord(char *, char *);
+char* reverse(char* ,int , int , int );
+char** stringcut (char *, char *, int);
+bool isIn (char*, char);
 
 int main()
 {
@@ -20,21 +25,69 @@ int main()
 
     free(sentence);
     free(newSentence);
+
+    system("pause");
     return 0;
+}
+
+bool isIn(char* s1, char s2)
+{
+    for(int i=0; i < strlen(s1); i++)
+    {
+        if(s1[i]==s2)
+            return true;
+    }
+    return false;
+}
+
+char* reverse(char* str, int len, int l, int r)
+{
+
+    if (l < 0 || r >= len || l > r)
+        return str;
+
+    while (l < r) {
+
+        char c = str[l];
+        str[l] = str[r];
+        str[r] = c;
+
+        l++;
+        r--;
+    }
+
+    return str;
+}
+
+char** stringcut (char *s1, char *s2, int n)
+{
+    char *word = (char*)malloc ((MAX_LENGTH+1)*sizeof(char)), *interract = (char*)malloc ((MAX_LENGTH+1)*sizeof(char));
+
+    for(int i=n; i<strlen(s1)+1; i++)
+    {
+
+        if(isalpha(s1[i]) || isIn(s2,s1[i]))
+        {
+            strcat(interract,s1[i]);
+            if()
+        }
+    }
 }
 
 void checkEachWord(char *s1, char *s2){
     char *word, *newWord = (char*) malloc((MAX_LENGTH+1)*sizeof(char));
     char firtsChar;
 
-    word = strtok (s1," ,.-");
+    word = strtok(s1," *',.-");
     while (word != NULL)
     {
+        //printf("%s\n",word);
         strcpy(newWord, word);
 
         // * 1 - 3 - 4
         firtsChar = newWord[0];
-        if(isalpha(firtsChar)){
+        if(isalpha(firtsChar))
+            {
             strcpy(&newWord[0], &newWord[1]);
             newWord[strlen(word)-1] = tolower(firtsChar);
             newWord[strlen(word)] = '\0';
