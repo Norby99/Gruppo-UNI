51375744e774245063b573f6ab392c4b3eb95bc5
Norbi Gabos <dont.vip@gmail.com>
2020-12-04 01:13:01 +0100

    Update Elaborato 2.c

--- Consegna-8/Elaborato 2.c
+++ Consegna-8/Elaborato 2.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_ARRAY_LENGHT 100
-#define MAX_REPETITIONS 2
+#define MAX_REPETITIONS 6
 
 typedef struct{
     int *element;
     int len;
 }myArray;
 
-void LookAndSay(int*, int);
 
+// ? Funzioni generiche
+void LookAndSay(myArray, int);
+
+
+// ? Metodi di myArray
 myArray newMyArray();
 void printMyArray(myArray);
 void appendMyArray(myArray*, int);
+void destroyer(myArray*);
 
 
 int main()
@@ -29,23 +33,49 @@ int main()
     return 0;
 }
 
-void LookAndSay(int *a, int repetitions){
+void LookAndSay(myArray a, int repetitions){
+    if(repetitions){
+        int j;
+        int cont;
+        myArray array = newMyArray();
 
-}
-
-void PrintArray(int* array){
-
+        for (int i=0; i < a.len; i++)
+        {
+            j = i+1;
+            cont = 0;
+            for (; j < a.len+1; j++)
+            {
+                if (j == a.len)
+                    cont++;
+                else{
+                    cont++;
+                    if (a.element[j] != a.element[j-1])
+                        break;
+                }
+            }
+            appendMyArray(&array, cont);
+            appendMyArray(&array, a.element[i]);
+            i = j-1;
+        }
+        printMyArray(array);
+        LookAndSay(array, --repetitions);
+    }
 }
 
 myArray newMyArray(){
     myArray a;
-    a.element = (int*)malloc(MAX_ARRAY_LENGHT * sizeof(int));
     a.len = 0;
+    a.element = (int*)malloc(a.len * sizeof(int));
     return a;
 }
 
 void appendMyArray(myArray *a, int el){
+    a->element = (int*)realloc(a->element, (a->len+1) * sizeof(int));
     a->element[a->len++] = el;
+}
+
+void destroyer(myArray *a){
+    free(a);
 }
 
 void printMyArray(myArray a){
