c351ca2ad24c584a8403993d6d5620ef7f4316f4
Sara Romeo <sara.romeo999@gmail.com>
2020-12-18 18:31:07 +0100

    Update main.c

--- Consegna-9/20201222_Elaborato02/main.c
+++ Consegna-9/20201222_Elaborato02/main.c
@@ -12,39 +12,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-/****************************** STRUTTURE ******************************/
-
-typedef struct {
-};
-
-/****************************** PROTOTIPI ******************************/
-
-int print1Menu();
-int print2Menu();
+#include "mypila.h"
 
 /****************************** MAIN ******************************/
 
 int main()
 {
     bool temp=true, tmp = true;
+    SACCHETTO_STATICO *ss;
+    SACCHETTO_DINAMICO *sd;
 
     while(tmp)
     {
         switch(print1Menu())
         {
-            case 1: // Allocazione indicizzata
+            case 1: // Allocazione indicizzata (vettore statico)
+            {
+                ss->pos=-1;  // init pos pila
                 while(temp)
                 {
                     switch(print2Menu())
                     {
                         case 1:
+                            pushStatic(ss);
                             break;
                         case 2:
+                            topStatic(popStatic(ss));
                             break;
                         case 3:
-                            break;
-                        case 4:
+                            topStatic(ss);
                             break;
                         case 0:
                             temp = false;
@@ -54,7 +50,32 @@ int main()
                             break;
                     }
                 }
-            case 2: // Strutture collegate
+            }
+            case 2: // Allocazione indicizzata (vettore dinamico)
+            {
+                initPila(sd);
+                while(temp)
+                {
+                    switch(print2Menu())
+                    {
+                        case 1:
+                            pushDinamic(sd);
+                            break;
+                        case 2:
+                            //topDinamic(sd);
+                            break;
+                        case 3:
+                            //popDinamic(sd);
+                            break;
+                        case 0:
+                            temp = false;
+                            break;
+                        default:
+                            printf("!!Valore inserito non valido!! Riprova.\n");
+                            break;
+                    }
+            }
+            case 3: // Strutture collegate
                 while(temp)
                 {
                     switch(print2Menu())
@@ -64,8 +85,6 @@ int main()
                         case 2:
                             break;
                         case 3:
-                            break;
-                        case 4:
                             break;
                         case 0:
                             temp = false;
@@ -82,19 +101,11 @@ int main()
                 printf("!!Valore inserito non valido!! Riprova.\n");
                 break;
             }
+        }
     }
 
-    destroyer();
+//    destroyer();
     return 0;
-}
-
-/****************************** FUNZIONI DEL MENU' ******************************/
-
-/****************************** GESTIONE STRUTTURE ******************************/
-
-void destroyer()
-{
-    free();
 }
 
 /****************************** MENU' ******************************/
@@ -104,19 +115,18 @@ int print2Menu()
     int choice;
 
     printf("\n**** MENU\' ****\n");
-    printf("1) \n");
-    printf("2) \n");
-    printf("3) \n");
-    printf("4) \n");
+    printf("1) Inserisci frutto in cima;\n");
+    printf("2) Elimina frutto affiorante;\n");
+    printf("3) Visualizza ultimo frutto aggiunto;\n");
     printf("0) Esci dal programma\n");
 
     printf("\n\nScegli una tra le seguenti operazioni: ");
     scanf(" %d", &choice);
     fflush(stdin);
 
-    while(choice<0 && choice!>4)
+    while(choice<0 && choice>3)
     {
-        if(choice<0 || choice!>4)
+        if(choice<0 || choice>3)
         {
             printf("\nL'operazione inserita non e\' presente nel menu\' di scelta, riprova: ");
             scanf(" %d", &choice);
@@ -132,23 +142,24 @@ int print1Menu()
     int choice;
 
     printf("\n**** MENU\' DI SCELTA TRA LE DUE VARIANTI ****\n");
-    printf("1) Allocazione indicizzata\n");
-    printf("2) Strutture collegate\n");
+    printf("1) Allocazione indicizzata (vettore statico)\n");
+    printf("2) Allocazione indicizzata (vettore dinamico)\n");
+    printf("3) Strutture collegate\n");
     printf("0) Esci dal programma\n");
 
     printf("\n\nScegli una tra le seguenti operazioni: ");
     scanf(" %d", &choice);
     fflush(stdin);
 
-    while(choice<0 && choice!>2)
+    while(choice<0 && choice>3)
     {
-        if(choice<0 || choice!>2)
+        if(choice<0 || choice>3)
         {
             printf("\nL'operazione inserita non e\' presente nel menu\' di scelta, riprova: ");
             scanf(" %d", &choice);
             fflush(stdin);
         }
     }
-    system("cls");
+    //system("cls");
     return choice;
 }
