058c394256391fce1fbb49806dd90b42f6cd9b08
DennyReggidori <72309051+DennyReggidori@users.noreply.github.com>
2020-12-19 16:45:36 +0100

    Update mypila.c

--- Consegna-9/20201222_Elaborato02/mypila.c
+++ Consegna-9/20201222_Elaborato02/mypila.c
@@ -9,6 +9,14 @@
 /****************************** FUNZIONI DEL MENU' ******************************/
 
 /** allocazione statica **/
+
+SACCHETTO_STATICO* initPila_Sta (SACCHETTO_STATICO *s)
+{
+    //s = (SACCHETTO_DINAMICO *) malloc(sizeof(SACCHETTO_DINAMICO)*1);
+    s->pos=-1;
+    return s;
+}
+
 void pushStatic (SACCHETTO_STATICO *s)
 {
     char *nome=(char *)malloc(MAX_NAME_LEN*sizeof(char));
@@ -73,6 +81,8 @@ void pushDinamic(SACCHETTO_DINAMICO *s)
     s->pos++;
     strncpy(s->spesa[s->pos].nome, nome, MAX_NAME_LEN);
     s->spesa[s->pos].prezzo = prezzo;
+    printf("----%s",s->spesa[s->pos].nome);
+    printf("----%d",s->pos);
 }
 
 void topDinamic (SACCHETTO_DINAMICO *s)
@@ -82,7 +92,7 @@ void topDinamic (SACCHETTO_DINAMICO *s)
         printf("ERRORE: PILA VUOTA\n");
     else
     {
-        printf("\nNome:\t%s", s->spesa[s->pos].nome);
+        printf("\nNome:\t %s", s->spesa[s->pos].nome);
         printf("\nPrezzo:\t%d\n", s->spesa[s->pos].prezzo);
     }
 }
@@ -96,40 +106,40 @@ void popDinamic(SACCHETTO_DINAMICO *s)
 
 /** allocazione statica**/
 
-bool TestPilaVuota1(SACCHETTO_STATICO s)        // restituisce TRUE se la pila è vuota, FALSE altrimenti
+bool TestPilaVuota1(SACCHETTO_STATICO s)        // restituisce TRUE se la pila Ã¨ vuota, FALSE altrimenti
 {
     return (s.pos == -1);
 }
 
-bool TestPilaPiena1(SACCHETTO_STATICO s)        // restituisceTRUE se la pila è piena, FALSE altrimenti
+bool TestPilaPiena1(SACCHETTO_STATICO s)        // restituisceTRUE se la pila Ã¨ piena, FALSE altrimenti
 {
     return (s.pos == (MAX_PILA-1));
 }
 
 /** allocazione dinamica **/
 
-SACCHETTO_DINAMICO* initPila (SACCHETTO_DINAMICO *s)
+SACCHETTO_DINAMICO* initPila_Din (SACCHETTO_DINAMICO *s)
 {
-    //s = (SACCHETTO_DINAMICO *) malloc(sizeof(SACCHETTO_DINAMICO)*1);
+    s = (SACCHETTO_DINAMICO *)malloc(sizeof(SACCHETTO_DINAMICO)*MAX_PILA);
     s->spesa = malloc(MAX_PILA*sizeof(FRUTTA));
     s->pos = -1;
     s->dim = MAX_PILA;
     return s;
 }
 
-bool TestPilaVuota2(SACCHETTO_DINAMICO s)       // restituisce TRUE se la pila è vuota, FALSE altrimenti
+bool TestPilaVuota2(SACCHETTO_DINAMICO s)       // restituisce TRUE se la pila Ã¨ vuota, FALSE altrimenti
 {
     return (s.pos == -1);
 }
 
-void PilaPiena(SACCHETTO_DINAMICO *s)            // aumenta le dimensioni della pila se questa è piena
+void PilaPiena(SACCHETTO_DINAMICO *s)            // aumenta le dimensioni della pila se questa Ã¨ piena
 {
     printf("STO RIALLOCANDO FORSE");
     if  (s->pos == (s->dim-1))
     {
         printf("STO RIALLOCANDO");
         s->dim = s->dim*2;                      // pila piena: raddoppia la dimensione del vettore
-        s->spesa = realloc(s->spesa, s->dim* sizeof(FRUTTA));
+        s->spesa = realloc(s->spesa, s->dim * sizeof(FRUTTA));
         // !!!!!! controlli realloc
     }
 }
